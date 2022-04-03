c7d5931b6bd7cc230a3fda4c3986b76f0344a429
Michal <noszczykmichal@wp.pl>
2022-04-03 17:34:04 +0200

    added new component NewToDo + state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './Components/ToDoList';
-
+import NewTodo from './Components/NewTodo';
+import {Todo } from './todo.models';
 
 const App: React.FC = () => {
-  const todos = [{ id: 't1', text: 'Finish the course' }, {id: 't2', text: 'Create a new app'}];
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (toDotext: string) => {
+    setTodos(prevTodos=> [...prevTodos, {id: Math.random().toString(), text: toDotext } ])
+  };
+  
   return (
     <div className="App">
-      <TodoList items={todos}/>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
     </div>
   );
 }
